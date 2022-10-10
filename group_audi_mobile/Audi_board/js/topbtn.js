9ff6dccb90942dcffb3b28c4a19517ef9c18dd9e
tkrsa1 <65238945+tkrsa1@users.noreply.github.com>
2022-10-10 12:56:45 +0900

    Delete group_audi_mobile directory

--- group_audi_mobile/Audi_board/js/topbtn.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('a[href="#"]').click(function(ignore){
-        ignore.preventDefault();
-    });
-    $('.topbtn').click(function(gotop){
-        gotop.preventDefault();
-        $('html, body').animate({scrollTop: 0},800);
-    });
-});
